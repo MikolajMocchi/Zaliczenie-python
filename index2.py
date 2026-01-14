3a8eab22227177757f87f7effc95b006a06cf8df
Mocchi <mikolajgolonka43@gmail.com>
2026-01-14 14:55:15 +0100

    Update index2.py

--- index2.py
+++ index2.py
@@ -5,7 +5,7 @@ def Parzysta(): #Zad 1
 	else:
 		print('liczba jest nieparzysta')
 
-def Wiek():
+def Wiek(): #zad 2
 	w = int(input('Wprowadz swój wiek: '))
 	if w in range(1, 10):
 		print('Jesteś dzieckiem')
@@ -14,13 +14,32 @@ def Wiek():
 	else:
 		print('Jesteś dorosły')
 
-def 
+def Licz(): #zad 3
+	list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+	for a in list:
+		if a > 10:
+			print(a)
+
+
+def Tabliczka(): #zad 4
+	for z in range(1, 11):
+		for x in range(1, 11):
+			wynik = z * x
+		print(f'{z} * {x} = {wynik }')
+
+
+def palidro(): #zad 5
+	print(nic)
+	
+
+
 
 print('> COS <')
 print()
 print('1.Sprawdź czy liczba jest parzysta')
 print('2.Czy jesteś dorosły czy nastolatkiem od zależności od wieku')
-print('3.')
+print('3.Liczneie powyżej 10')
+print('4.Tabliczka mnożenia dla liczb od 1 do 10')
 xd = int(input('Wybierz opcie: '))
 
 if xd == 1:
@@ -28,3 +47,12 @@ if xd == 1:
 
 if xd == 2:
 	print(Wiek())
+
+if xd == 3:
+	print(Licz())
+
+if xd == 4:
+	print(Tabliczka())
+
+if xd == 5:
+	print(palidro())
