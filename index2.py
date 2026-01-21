7497e715c7b93f62e56779a20dd0931ca58d7431
Mocchi <mikolajgolonka43@gmail.com>
2026-01-21 07:30:21 +0100

    Update index2.py

--- index2.py
+++ index2.py
@@ -54,6 +54,46 @@ def Ocena():
     procenty = pro / m * 100  
     
     print(f"{procenty}", "%")
+    
+def BMI():
+    waga = float(input("Podaj wagę w kg: "))
+    wzrost = float(input("Podaj wzrost w m: "))
+    
+    bmi = waga / (wzrost ** 2)
+    
+    print(f"Twoje BMI wynosi: {bmi}")
+    
+    if bmi < 18.5:
+        print("Masz niedowagę")
+    elif bmi < 25:
+        print("Twoja waga jest w normie")
+    else:
+        print("Masz nadwagę")
+        
+def Trojkat():
+    a = float(input("Podaj długość pierwszego boku: "))
+    b = float(input("Podaj długość drugiego boku: "))
+    c = float(input("Podaj długość trzeciego boku: "))
+    
+    if a > 0 and b > 0 and c > 0:
+        if a + b > c and a + c > b and b + c > a:
+            print("Z podanych boków można zbudować trójkąt")
+        else:
+            print("Z podanych boków NIE można zbudować trójkąta")
+    else:
+        print("Długości boków muszą być większe od 0")
+        
+def najliczba():
+    a = float(input("Podaj pierwszą liczbę: "))
+    b = float(input("Podaj drugą liczbę: "))
+    c = float(input("Podaj trzecią liczbę: "))
+    
+    if a >= b and a >= c:
+        print("Największa liczba to:", a)
+    elif b >= a and b >= c:
+        print("Największa liczba to:", b)
+    else:
+        print("Największa liczba to:", c)
 
 print('> COS <')
 print()
@@ -65,6 +105,8 @@ print('5.Sprawdz czy słowo jest palindromem')
 print('6.Celciusz na Fahrenheit')
 print('7.Fahrenheit na Celciusz')
 print('8.Ile ci wychodzi procent z punktów ze sprawdzianu')
+print('9.Sprawdź swoje BMI')
+print('10.Sprawdź czy wybrane liczby staną się trójkątem')
 xd = int(input('Wybierz opcie: '))
 
 if xd == 1:
@@ -90,6 +132,15 @@ elif xd == 7:
 
 elif xd == 8:
 	print(Ocena())
+
+elif xd == 9:
+    print(BMI())
+    
+elif xd == 10:
+    print(Trojkat())
+    
+elif xd == 11:
+    print(najliczba())
 	
 
 else:
